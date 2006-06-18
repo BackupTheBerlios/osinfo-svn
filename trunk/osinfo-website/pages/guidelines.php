4aef560fe2005921469719320d0b2187bf909368
anmaster <anmaster@58511cf3-be0f-0410-bd7d-b85c727eece6>
2006-06-18 15:41:31 +0000

    Changed website. I hope this works.

--- trunk/osinfo-website/pages/guidelines.php
+++ trunk/osinfo-website/pages/guidelines.php
@@ -8,7 +8,7 @@ else {
 ?>
 <?php echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?".">"; ?>
 <?php 
-	require_once("res/includes/menu.php");
+	require_once("../res/includes/menu.php");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -20,11 +20,14 @@ else {
 
 <body>
 <?php
-	print_menu(".", "/index.php", "");
-
+	print_menu("..", "/pages/guidelines.php", "Coding guidelines");
 ?>
 	<div class="text">
-		<p class="medium">Osinfo is a script for showing information about Linux systems. It is written in bash. Osinfo supports both colour coded console output and exporting to XML.</p>
+		<p class="medium">Some coding guidelines:</p>
+		<ul>
+			<li>Put documentation comments of a function right before it. Not inside it.</li>
+			<li>Avoid UUOC (Useless Use of Cat (cat foo | grep bar). See <a href="http://www.ruhr.de/home/smallo/award.html">http://www.ruhr.de/home/smallo/award.html</a>).</li>
+		</ul>
 	</div>
 	<hr class="hide" />
 	<div class="center">
---
+++ trunk/osinfo-website/res/include/menu.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+Prints a menuitem.
+Parameters:
+$prefix       Text to put before the item
+$title        The text of the link
+$relativeroot A string like "../.." that is the relative path to the root level.
+$url          The path to the link target from the root level.
+$me           The path to the file this menuitem will be used in.
+$islast       If true this is the last menuitem.
+Returns:
+Nothing
+*/
+function print_menuitem($prefix, $title, &$relativeroot, $url, &$me, $islast) {
+	// Is this a link to the file this item will be used in?
+	if ($me == $url) {
+		// If true: Print $title bold.
+		echo $prefix."<strong>".$title."</strong>";
+	} else {
+		// If false: Print a link.
+		echo $prefix."<a href=\"".$relativeroot.$url."\">".$title."</a>";
+	}
+
+	// No linebreak if this is the last item.
+	if (! $islast)
+		echo "<br />";
+}
+
+
+/*
+Prints the title and the menu.
+Parameters:
+ $relativeroot A string like "../.." that is the relative path to the root level.
+ $me           The path to the file this menu will be used in.
+Returns:
+  Nothing
+*/
+function print_menu($relativeroot, $me, $title) {
+	?>
+		<h1 class="siteheader">
+			<a name="top" id="top" />osinfo
+		</h1>
+		<hr class="hide" />
+		<div class="menu">
+			<p><strong>Menu</strong><br />
+				<?php
+					$bullet="<img src=\"".$relativeroot."/res/images/bullet.gif\" alt=\"-\" />";
+					$lev1="&nbsp;&nbsp;".$bullet."&nbsp;";
+					$lev2="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".$bullet."&nbsp;";
+
+					print_menuitem("", "Home",
+					               $relativeroot, "/index.php",                                                      $me, false);
+					print_menuitem("",            "Project page",
+					               "",            "http://developer.berlios.de/projects/osinfo/",                    $me, false);
+					print_menuitem("",            "Browse the SVN Tree",
+					               "",            "http://svn.berlios.de/wsvn/osinfo/",                              $me, false);
+					print_menuitem("",            "Downloads",
+					               "",            "http://developer.berlios.de/project/showfiles.php?group_id=6372", $me, false);
+					print_menuitem("",            "Coding guidelines",
+					               $relativeroot, "/pages/guidelines.php",                                           $me, true);
+			?>
+		</p>
+		<a href="http://developer.berlios.de" title="BerliOS Developer">
+			<img src="http://developer.berlios.de/bslogo.php?group_id=6372" class="berliosimage" alt="BerliOS Developer Logo" />
+		</a>
+		</div>
+	<?php
+}
+?>
+
+
+
+?>
