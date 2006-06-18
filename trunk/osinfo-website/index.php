e93a5cf40ac28521435b1fd4e9b8c48e705ed4bc
anmaster <anmaster@58511cf3-be0f-0410-bd7d-b85c727eece6>
2006-06-18 18:26:57 +0000

    Some changes to web page

--- trunk/osinfo-website/index.php
+++ trunk/osinfo-website/index.php
@@ -24,7 +24,8 @@ else {
 
 ?>
 	<div class="text">
-		<p class="medium">Osinfo is a script for showing information about Linux systems. It is written in bash. Osinfo supports both colour coded console output and exporting to XML.</p>
+		<p class="medium">Osinfo is a script for showing information about Linux and BSD systems. It is written in bash. Osinfo supports both colour coded console output and exporting to XML.</p>
+		<p class="medium">Where it is possible it only uses standard *nix/POSIX tools. Dome information is unavailable (but quite common) packages, but the availability of these are tested, and no error messaged are produced if these rarer tools are missing.</p>
 	</div>
 	<hr class="hide" />
 	<div class="center">
