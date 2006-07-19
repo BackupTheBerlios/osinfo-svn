b4176402f8bcd20dd23cb8afa9b099abc0646a00
anmaster <anmaster@58511cf3-be0f-0410-bd7d-b85c727eece6>
2006-07-19 08:58:45 +0000

    Updated DTD, fixed some tabs in comments and fixed svn:eol-style and svn:ignore in a few places

--- trunk/osinfo/install-gentoo.sh
+++ trunk/osinfo/install-gentoo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo ln -s $(pwd)/init.d/osinfo.gentoo /etc/init.d/osinfo
+sudo ln -s "$(pwd)/init.d/osinfo.gentoo" /etc/init.d/osinfo
 make dist-all-in-one
 sudo mv dist/osinfo/osinfo-all-in-one /usr/local/bin/osinfo
-sudo ln -s $(pwd)/osinfo.conf /etc/
+sudo ln -s "$(pwd)/osinfo.conf" /etc/
