730dba42b906ab9ed20a47563ab42163251e220b
lamikae <lamikae@58511cf3-be0f-0410-bd7d-b85c727eece6>
2006-08-13 14:33:03 +0000

    tweaked the config file and init scripts

--- trunk/osinfo/install-gentoo.sh
+++ trunk/osinfo/install-gentoo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo ln -s "$(pwd)/init.d/osinfo.gentoo" /etc/init.d/osinfo
+make clean
 make dist-all-in-one
 sudo mv dist/osinfo/osinfo-all-in-one /usr/local/bin/osinfo
 sudo ln -s "$(pwd)/osinfo.conf" /etc/
